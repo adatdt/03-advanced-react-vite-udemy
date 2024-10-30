0a9280c3a44c4fe2ba5fde704dd4048541625cbd
unknown <adat.nutech@gmail.com>
2024-10-30 16:09:37 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import Starter from './tutorial/07-useRef/starter/01-useRef-basics'
+// import Starter from './tutorial/07-useRef/starter/01-useRef-basics'
+// import Starter from './tutorial/01-useState/starter/01-error-example.jsx';
+// import Starter from './tutorial/01-useState/starter/02-useState-basics.jsx';
+import Starter from './tutorial/01-useState/starter/03-useState-array.jsx';
+
+
 function App() {
   return (
     <div className='container'>
