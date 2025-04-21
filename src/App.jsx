f605d4054552c40af27ad492077d7533fc7584e8
unknown <adat.nutech@gmail.com>
2025-04-21 16:01:20 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,11 @@
 
 // import Starter from './tutorial/03-conditional-rendering/starter/01-multiple-returns-basics'
 // import Starter from './tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data'
-import Starter from "./tutorial/03-conditional-rendering/starter/03-hooks-rule"
+// import Starter from "./tutorial/03-conditional-rendering/starter/03-hooks-rule"
+// import Starter from "./tutorial/03-conditional-rendering/starter/04-short-circuit-overview"
+// import Starter from "./tutorial/03-conditional-rendering/starter/05-short-circuit-examples"
+import Starter from "./tutorial/03-conditional-rendering/starter/06-toggle-challenge"
+
 
 function App() {
   return (
