de3cf168bec0b84b7d58735f8e5b4c3d8690d159
unknown <adat.nutech@gmail.com>
2025-05-06 16:40:17 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,20 @@
 // import Starter from './tutorial/02-useEffect/starter/02-useEffect-basics.jsx';
 // import Starter from './tutorial/02-useEffect/starter/03-multiple-effects.jsx';
 // import Starter from './tutorial/02-useEffect/starter/04-fetch-data.jsx';
+import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function";
 
 // import Starter from './tutorial/03-conditional-rendering/starter/01-multiple-returns-basics'
 // import Starter from './tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data'
 // import Starter from "./tutorial/03-conditional-rendering/starter/03-hooks-rule"
 // import Starter from "./tutorial/03-conditional-rendering/starter/04-short-circuit-overview"
 // import Starter from "./tutorial/03-conditional-rendering/starter/05-short-circuit-examples"
-import Starter from "./tutorial/03-conditional-rendering/starter/06-toggle-challenge"
+// import Starter from "./tutorial/03-conditional-rendering/starter/06-toggle-challenge"
+// import Starter from "./tutorial/03-conditional-rendering/starter/07-user-challenge"
+
+// import Final from "./tutorial/04-project-structure/final/Example"
+
+// import Final from "./Navbar/Navbar"
+// import {About, Home } from "./Pages";
 
 
 function App() {
@@ -23,6 +30,7 @@ function App() {
     <div className='container'>
         {/* <h2>advance react</h2> */}
         <Starter />
+        {/* <Final /> */}
     </div>
   );
 }
--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from 'react';
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const[toggle,setToggle]  = useState(false)
+  console.log('render')
+  return (<div>
+    <button className='btn' onClick={()=>setToggle(!toggle)}>Togle Component</button>
+    {toggle &&<RandomComponent />}
+  </div>);
 };
+
+const RandomComponent =()=>{
+  useEffect(()=>{
+    // console.log('hmmm, this is interesting')
+    // const intID = setInterval(()=>{
+    //   // console.log("hello from interval")
+    // },1000)
+
+    // return ()=> {
+    //   clearInterval(intID)
+    //   console.log('clean up')
+    // }
+
+    const someFunc = ()=>{
+
+    }
+    window.addEventListener('scroll', someFunc)
+    return ()=>window.removeEventListener('scroll', someFunc)
+  },[])
+  return <h1>Hello there</h1>
+}
 
 export default CleanupFunction;
